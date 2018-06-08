72b668ff2a12100d2eb456c3a3f1fd8a6cec559d
abhiwin2010 <abhiwin2010@gmail.com>
2018-06-08 18:48:17 +0530

    smart contract updated

--- contracts/Hashes.sol
+++ contracts/Hashes.sol
@@ -3,13 +3,42 @@ pragma solidity 0.4.20;
 contract Hashes{
 
 	address public owner;
+	uint256 public min_eth;
+	uint256 public total_eth;
+	uint256 public no_of_files;
+	address[] public clients;
 	
+	struct client {
+		uint256 eth_amt;
+		string hash;
+	}
 
-	func Hashes() public {
+	function() public payable {}
+
+	func Hashes(uint256 _mineth) public {
 		owner = msg.sender;
+		if(_mineth != 0) min_eth = _mineth;
 	}
 
 	func kill() public {
 		if(msg.sender == owner) selfdestruct(owner);
 	}
+
+	func upload(string hash_str ) public payable {
+		require(!checkClientExists(msg.sender));
+		require(msg.value >= min_eth);
+
+		clientInfo[msg.sender].eth_amt = msg.value;
+		clientInfo[msg.sender].hash = hash_str;
+		no_of_files++;
+		total_eth += msg.value;
+	}
+
+	func checkClientExists(address client) public constant returns(bool){
+		for(uint256 i = 0; i < clients.length; i++){
+			if(clients[i] == client) return true;
+		}
+		return false;
+	}
+
 }
